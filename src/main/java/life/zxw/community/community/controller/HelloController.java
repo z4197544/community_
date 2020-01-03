e4ec99854b1500bc4885e5f5449e884d7af4b099
zhaoxuewen <834197544@qq.com>
2020-01-03 10:08:55 +0800

    first

--- src/main/java/life/zxw/community/community/controller/HelloController.java
+++ src/main/java/life/zxw/community/community/controller/HelloController.java
@@ -5,14 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.print.DocFlavor;
-
-@Controller
-public class HelloController {
-
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name")String name,Model model){
-        model.addAttribute("name",name);
-        return "hello";
-    }
-}
+//@Controller
+//public class HelloController {
+//    @GetMapping("/hello")
+//    public String hello(@RequestParam(name = "name")String name,Model model){
+//        model.addAttribute("name",name);
+//        return "index";
+//    }
+//}
---
+++ src/main/java/life/zxw/community/community/controller/IndexController.java
@@ -0,0 +1,14 @@
+package life.zxw.community.community.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
